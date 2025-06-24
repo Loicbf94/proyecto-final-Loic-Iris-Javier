bc01f4be72067cb7ab82695fc7ebe971593ecdb4
Luna <loic123@hotmail.es>
2025-06-24 19:57:56 +0200

    script con en_core_webt

--- src/app.py
+++ src/app.py
@@ -6,6 +6,12 @@ from sklearn.neighbors import NearestNeighbors
 import ast
 import spacy
 
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    
 nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
 
 def clean_text_spacy(text, pos_tags):
